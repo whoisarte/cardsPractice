b8bdd249af878b516cd81b6cb4609179fe7f3401
Artemio Pánuco <arte.solo.arte@hotmail.com>
2021-09-19 21:51:14 -0500

    Initial Commit

---
+++ cardsPractice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  cardsPractice
+//
+//  Created by Artemio on 19/09/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
